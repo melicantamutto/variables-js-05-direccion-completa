ad06588c96bb4562abd7d21bccb4e3643d5f747d
MELI CANTAMUTTO <MELICANTAMUTTO@gmail.com>
2020-11-21 12:52:49 -0300

    ejercicio js

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log('Dirección completa')
+
+alert('Hola')
+
+const calle = prompt('Ingresa el nombre de tu calle');
+const numeracion = prompt('Ingresa la numeracion de tu casa');
+const departamento = prompt('Ingresa el número de departamento');
+const codigoPostal = prompt('Ingresa tu código postal');
+const ciudad = prompt('Ingresa tu ciudad');
+const pais = prompt('Ingresa tu país')
+
+alert(`La dirección que ha ingresado es: ${calle} ${numeracion} ${departamento}, ${codigoPostal}, ${ciudad}, ${pais}.`)
